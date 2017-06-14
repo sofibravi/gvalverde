1aa5708e9d2b7b8ec5fd921d153265d75a4a8296
German Valverde <valverdeauboneg@gmail.com>
2017-06-14 16:00:53 -0300

    '0.1.0'

---
+++ src/calculadorais/Calculadora.java
@@ -0,0 +1,9 @@
+/**
+ * @author: German Valvarde
+ * @version: 0.1.0
+ */ 
+package calculadorais;
+
+public interface Calculadora {
+    public double calcula(double x, double y);
+    }
---
+++ src/calculadorais/CalculadoraIS.java
@@ -0,0 +1,14 @@
+
+/**
+ * @author: German Valvarde
+ * @version: 0.1.0
+ */ 
+package calculadorais;
+public class CalculadoraIS {
+
+    
+    public static void main(String[] args) {
+       Calculadora c;   
+       
+}
+}
